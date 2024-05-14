e5a787fd91d5fcb995ab193bdbd7f7bf99400ec6
Rami <138556019+rami242424@users.noreply.github.com>
2024-05-14 17:00:35 +0900

    #7.2 Redirect and Rewrite - 방법1 API next.config에 적기

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,13 @@
 import Seo from "@/components/Seo";
 import { useEffect, useState } from "react";
 
-const API_KEY = "10923b261ba94d897ac6b81148314a3f"
-
-
 export default function Home(){
     const [movies, setMovies] = useState();
     useEffect(() => {
         (async() => {
             const {results} = await (
                 await fetch(
-                `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+                `/api/movies`
                 )
             ).json();
             // console.log(results);
